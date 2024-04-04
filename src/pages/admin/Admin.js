54ece70783bacaa36e079807381f326cd3a1932e
bekzoddr <149781755+bekzoddr@users.noreply.github.com>
2024-04-05 02:33:39 +0500

    login

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import admin from "../../assets/images/purple-free-admin-dashboard-e1597995165506.png";
 function Admin() {
-  return <div>Admin</div>;
+  return (
+    <div>
+      <img
+        style={{ width: "100vw", height: "100vh", objectFit: "cover" }}
+        src={admin}
+        alt=""
+      />
+    </div>
+  );
 }
 
 export default Admin;
