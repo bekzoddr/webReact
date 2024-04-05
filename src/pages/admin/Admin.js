0224bf8c3e94f7a4377aa2be2e37085e27e49b8c
bekzoddr <149781755+bekzoddr@users.noreply.github.com>
2024-04-05 14:01:18 +0500

    login

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -2,7 +2,7 @@ import React from "react";
 import admin from "../../assets/images/purple-free-admin-dashboard-e1597995165506.png";
 function Admin() {
   return (
-    <div>
+    <div style={{ margin: "100px 0" }}>
       <img
         style={{ width: "100vw", height: "100vh", objectFit: "cover" }}
         src={admin}
